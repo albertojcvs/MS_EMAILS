e9869e0bc0f6611469b3910395fe6cd083786fb7
Alberto Jose <albertojcvs@gmail.com>
2021-12-20 09:20:00 -0300

    add the configs of mail

--- src/configs/mail.ts
+++ src/configs/mail.ts
@@ -1,8 +1,8 @@
-export const configs = {
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS
-    }
-}
+export default  {
+  host: process.env.MAIL_HOST || "",
+  port: Number(process.env.MAIL_PORT) || 2525,
+  auth: {
+    user: process.env.MAIL_USER || "",
+    pass: process.env.MAIL_PASS || "",
+  },
+};
